b6f492352ae07d33e96d5faede35ad42a4e7476f
zeel <zeel02@gmail.com>
2020-10-05 22:07:36 -0400

    Total refactor

--- pin-fixer.js
+++ pin-fixer.js
@@ -1,14 +1,17 @@
 class PinFixer {
-	static get minScale() { return 0.1; }
-	static get maxScale() { return CONFIG.Canvas.maxZoom; }
+	/** @type {Number} */ 
+	static get minCanvScale() { return 0.1; }
+	/** @type {Number} */
+	static get maxCanvScale() { return CONFIG.Canvas.maxZoom; }
 
-	static get flags() { return canvas.scene.data.flags; }
+	/** @type {object} */
+	static get flags()        { return canvas.scene.data.flags; }
 
-	static get enabled() { return !!this.flags.enable; }
-	static get zoomFloor() { return this.flags.pinfix.zoomFloor ?? this.minScale; }
-	static get zoomCeil() { return this.flags.pinfix.zoomFloor ?? this.maxScale; }
-	static get minScale() { return this.flags.pinfix.minScale ?? 1; }
-	static get maxScale() { return this.flags.pinfix.maxScale ?? 1; }
+	static get enabled()      { return Boolean(this.flags.pinfix?.enable); }
+	static get zoomFloor()    { return  Number(this.flags.pinfix?.zoomFloor ?? this.minCanvScale); }
+	static get zoomCeil()     { return  Number(this.flags.pinfix?.zoomCeil  ?? this.maxCanvScale); }
+	static get minScale()     { return  Number(this.flags.pinfix?.minScale  ?? 1); }
+	static get maxScale()     { return  Number(this.flags.pinfix?.maxScale  ?? 1); }
 
 	static reciprocal(number) { return 1 / number; }
 
@@ -34,8 +37,9 @@ class PinFixer {
 	}
 
 	static scaleNotes(scale) {
+		const scaled = this.noteScaleConfigured(scale);
 		canvas.notes.objects.children.forEach(note => 
-			this.scaleNote(note, this.noteScaleConfigured(scale))
+			this.scaleNote(note, scaled)
 		);
 	}
 
@@ -54,42 +58,63 @@ class PinFixer {
 
 	static canvasPan(canvas, pan) {
 		this.scaleNotes(pan.scale);
+
+		const basicScale = this.noteScaleBasic(pan.scale);
+		this.scalePinCushion(basicScale);
+		this.scalePoiTp(basicScale);
 	}
-}
+	static renderPinCushionHUD(hud, html, data) {
+		const basicScale = this.noteScaleBasic(canvas.stage.scale.x);
+		this.scalePinCushion(basicScale);
+	}
+	static renderPoiTpHUD(hud, html, data) {
+		const basicScale = this.noteScaleBasic(canvas.stage.scale.x);
+		this.scalePoiTp(basicScale);
+	}
 
-Hooks.on("canvasPan", (...args) => PinFixer.canvasPan(...args));
+	static updateScene(scene, data, options) {
+		this.canvasPan(canvas, { scale: canvas.stage.scale.x });
+	}
 
+	static renderSceneConfig(sceneConfig, html, data) {
+		html.find(".form-group").last().after(this.template);
+	}
 
-Hooks.on("renderSceneConfig", (sceneConfig, html, data) => {
-	const flags = data.entity.flags;
-	const template = `
+	static get template() {
+		return `
 		<hr>
 		<h3 class="form-header"><i class="fas fa-bookmark"></i> ${game.i18n.localize("pinfix.title")}</h3>
 		<p class="notes">${game.i18n.localize("pinfix.description")}</p>
 		<div class="form-group">
 			<label>${game.i18n.localize("pinfix.enable.name")}</label>
-			<input type="checkbox" name="flags.pinfix.enable" data-dtype="Boolean" ${flags.pinfix?.enable ? "checked":""}>
+			<input type="checkbox" name="flags.pinfix.enable" data-dtype="Boolean"${this.enable ? " checked" : ""}>
 			<p class="notes">${game.i18n.localize("pinfix.enable.desc")}</p>
 		</div>
 		<div class="form-group">
 			<label>${game.i18n.localize("pinfix.minScale.name")}</label>
-			<input type="text" name="flags.pinfix.minScale" data-dtype="Number" value="${flags.pinfix?.minScale ?? 1}">
+			<input type="text" name="flags.pinfix.minScale" data-dtype="Number" value="${this.minScale}">
 			<p class="notes">${game.i18n.localize("pinfix.minScale.desc")}</p>
 		</div>
 		<div class="form-group">
 			<label>${game.i18n.localize("pinfix.maxScale.name")}</label>
-			<input type="text" name="flags.pinfix.maxScale" data-dtype="Number" value="${flags.pinfix?.maxScale ?? 1}">
+			<input type="text" name="flags.pinfix.maxScale" data-dtype="Number" value="${this.maxScale}">
 			<p class="notes">${game.i18n.localize("pinfix.maxScale.desc")}</p>
 		</div>
 		<div class="form-group">
 			<label>${game.i18n.localize("pinfix.zoomFloor.name")}</label>
-			<input type="text" name="flags.pinfix.zoomFloor" data-dtype="Number" value="${flags.pinfix?.zoomFloor ?? .1}">
+			<input type="text" name="flags.pinfix.zoomFloor" data-dtype="Number" value="${this.zoomFloor}">
 			<p class="notes">${game.i18n.localize("pinfix.zoomFloor.desc")}</p>
 		</div>
 		<div class="form-group">
 			<label>${game.i18n.localize("pinfix.zoomCeil.name")}</label>
-			<input type="text" name="flags.pinfix.zoomCeil" data-dtype="Number" value="${flags.pinfix?.zoomCeil ?? 3}">
+			<input type="text" name="flags.pinfix.zoomCeil" data-dtype="Number" value="${this.zoomCeil}">
 			<p class="notes">${game.i18n.localize("pinfix.zoomCeil.desc")}</p>
 		</div>`;
-	html.find(".form-group").last().after(template);
-});
+	}
+}
+
+Hooks.on("canvasPan", (...args) => PinFixer.canvasPan(...args));
+Hooks.on("renderPinCushionHUD", (...args) => PinFixer.renderPinCushionHUD(...args));
+Hooks.on("renderPoiTpHUD", (...args) => PinFixer.renderPoiTpHUD(...args));
+Hooks.on("renderSceneConfig", (...args) => PinFixer.renderSceneConfig(...args));
+Hooks.on("updateScene", (...args) => PinFixer.updateScene(...args));
