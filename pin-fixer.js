43d9245c1ff4a4a1fb75f943cab299b2d5fa202a
zeel <zeel02@gmail.com>
2020-11-27 19:48:42 -0500

    Fixed issue where pins a user did not have permissions to view could be seen.

--- pin-fixer.js
+++ pin-fixer.js
@@ -160,6 +160,7 @@ class PinFixer {
 	 * @memberof PinFixer
 	 */
 	static shouldHide(note, scale) {
+		if (!note._canView()) return true;
 		const flags = note.data.flags?.pinfix;
 		if (!flags || this.onNotesLayer) return false;
 		return flags.minZoomLevel > scale || flags.maxZoomLevel < scale;
