ce1f9bec10c33c34f533b31ab313562287d3400e
p4535992 <tentimarco0@gmail.com>
2022-08-27 10:26:50 +0200

    beta

--- src/module/socket.js
+++ src/module/socket.js
@@ -1,16 +1,18 @@
 import CONSTANTS from "./constants.js";
 import API from "./api.js";
 import { debug } from "./lib/lib.js";
-import { setSocket } from "../final-blow.js";
-export let finalBlowSocket;
+import { setSocket } from "../main.js";
+
+export let pinFixerSocket;
+
 export function registerSocket() {
-    debug('Registered finalBlowSocket');
-    if (finalBlowSocket) {
-        return finalBlowSocket;
+    debug('Registered pinFixerSocket');
+    if (pinFixerSocket) {
+        return pinFixerSocket;
     }
     //@ts-ignore
-    finalBlowSocket = socketlib.registerModule(CONSTANTS.MODULE_NAME);
-    finalBlowSocket.register('renderDialogMMMForFinalBlow', (...args) => API.renderDialogMMMForFinalBlowArr(...args));
-    setSocket(finalBlowSocket);
-    return finalBlowSocket;
+    pinFixerSocket = socketlib.registerModule(CONSTANTS.MODULE_NAME);
+    // pinFixerSocket.register('renderDialogMMMForFinalBlow', (...args) => API.renderDialogMMMForFinalBlowArr(...args));
+    setSocket(pinFixerSocket);
+    return pinFixerSocket;
 }
